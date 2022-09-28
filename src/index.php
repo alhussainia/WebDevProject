559f58aba3ef063a35692f48ac81c82b97fe5d6b
knappenbergerk <knappenbergerk@etown.edu>
2022-09-28 18:01:55 -0400

    Added title

--- src/index.php
+++ src/index.php
@@ -2,6 +2,7 @@
 <html lang="en">
 
 <head>
+    <title>Elizabethtown College's Pet Profiles</title>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">

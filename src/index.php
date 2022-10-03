c241d824ce4470a37aa4557cad2554fda1cf822c
olivyahvanek <[Lvanek1999@gmail.com]>
2022-10-03 16:46:18 -0400

    adding images

--- src/index.php
+++ src/index.php
@@ -13,14 +13,15 @@
     <?PHP require("navbar.php");?>
 <body>
     
-    <div style="width: 45%; float: left;"}>
+    <div style="width: 35%; float: left;"}>
         <h2>About Us</h2>
-            <p>This website was created for students of Elizabethtown College to enter information about their pets, in the form of a profile. This site will be used to keep track of all of the pets that live on campus, in the case of one going missing, so other students can keep a lookout for the missing animal to help find them. </p>
-    </div>
+            <p>This website was created for students of Elizabethtown College to enter information about their pets, in the form of a profile. This site will be used to keep track of all of the pets that live on campus, in the case of one going missing, so other students can keep a lookout for the missing animal to help find them. This website will be used by any students and faculty of Elizabethtown College to help keep track of any and all animals that are living on campus with students. </p>
+            <p>This website was created for students of Elizabethtown College to enter information about their pets, in the form of a profile. This site will be used to keep track of all of the pets that live on campus, in the case of one going missing, so other students can keep a lookout for the missing animal to help find them. This website will be used by any students and faculty of Elizabethtown College to help keep track of any and all animals that are living on campus with students. </p>
+        </div>
 
-    <div style="width: 45%; float: right;"> 
+    <div style="width: 60%; float: right;"> 
         <h2> Pet Spotlight of the Week: </h2>
-        <img src="../imgs/EtownLogo.jpg" style="width: 100%;">
+        <img src="../imgs/Kyra's Pet Spotlight.jpg" style="width: 100%;">
     </div>
     
 

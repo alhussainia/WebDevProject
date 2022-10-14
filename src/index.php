1adfac1a826f79f18646d9e7e25da96cc22d1908
olivyahvanek <[Lvanek1999@gmail.com]>
2022-10-14 11:02:10 -0400

    changing colors to match etown branding

--- src/index.php
+++ src/index.php
@@ -13,12 +13,12 @@
     <?PHP require("navbar.php");?>
 <body>
     
-    <div style="width: 30%; float: left; margin-left: 15px; color: blue;">
+    <div style="width: 30%; float: left; margin-left: 15px; color: #004B98;">
         <h2>About Us</h2>
             <p>This website was created for students of Elizabethtown College to enter information about their pets, in the form of a profile. This site will be used to keep track of all of the pets that live on campus, in the case of one going missing, so other students can keep a lookout for the missing animal to help find them. This website will be used by any students and faculty of Elizabethtown College to help keep track of any and all animals that are living on campus with students. </p>
         </div>
 
-    <div style="width: 60%; float: right; margin-right: 15px;"> 
+    <div style="width: 60%; float: right; margin-right: 15px; color: #004B98;"> 
         <h2> Pet Spotlight of the Week: </h2>
         <img src="../imgs/Kyra's Pet Spotlight.jpg" style="width: 100%;">
     </div>

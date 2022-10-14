aa9e3640ecfe94d6e4620ea921b7833f4ba9698d
knappenbergerk <knappenbergerk@etown.edu>
2022-10-13 21:43:28 -0400

    Edited the navbar to include lost and found pages

--- src/navbar.php
+++ src/navbar.php
@@ -5,7 +5,8 @@
         <div class="navbar-nav">
             <a class="nav-item nav-link active" href="index.php">| Home |<span class="sr-only"></span></a>
             <a class="nav-item nav-link" href="petprofiles.php"> Pet Profiles |</a>
-            <a class="nav-item nav-link" href="lostandfound.php"> Lost & Found |</a>
+            <a class="nav-item nav-link" href="lost.php"> Lost |</a>
+            <a class="nav-item nav-link" href="found.php"> Found |</a>
             <a class="nav-item nav-link disabled" href="petapplications.php"> Pet Applications |</a>
             <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
             <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>

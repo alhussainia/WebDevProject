555d2ca84d82dcb13d4c1cfc61c1647deb11dc6d
knappenbergerk <knappenbergerk@etown.edu>
2022-10-26 11:17:05 -0400

    Edited the found.php file

--- src/found.php
+++ src/found.php
@@ -17,12 +17,11 @@
         <h2 style="text-align: center">Found Pets</h2>
         <p>The purpose of this page is for students to report any found and caught animals on campus.</p>
 
-        <div style= "text-align: center; font-size: 18px; color: #004B98;"> If you have lost your pet, click <a class="nav-item nav-link" href="lost.php">here</a> to submit a lost report.
-
+        
     </div>
 
         <div>
-        <h2> Found Pets: </h2>
+        <h3> Found Pets: </h3>
         <img src="../imgs/Angel.png" style="height:250px; width:200px; color: #004B98;">
         <p>Name: Angel Closs <BR>Found: The Dell on 9/18/22</p>
         </div>
@@ -30,7 +29,7 @@
 
     
         <div>
-        <h2> Did you find a pet? </h2>
+        <h3> Did you find a pet? </h3>
         <form method=post action=students.php>
         Pet First Name: <input name="first_name"><BR/>
         Pet Description: <input name="description"><BR/>
@@ -41,6 +40,11 @@
 
     </div>
     </div>
+
+    <footer>
+        <p>If you have lost your pet, click <a class="nav-item nav-link" href="lost.php">here</a> to submit a lost report.<p>
+</footer>
+
 
 </body>
 

1c64210dac0467d27f22db65be66d061be2e65d9
knappenbergerk <knappenbergerk@etown.edu>
2022-10-26 21:55:07 -0400

    Edited pet profiles page

--- src/petprofiles.php
+++ src/petprofiles.php
@@ -10,15 +10,15 @@
 </head> 
 
 <body>
-    <div>
-     
+<div>
+    <div> 
     <?PHP require("navbar.php");?>
     <h2> Pets On Campus</h2>
     <p> Here you can see all the pets that have had profiles created so far! </p>
     <h3> Cats </h3>
     </div>
 
-    <div id ="profiles">
+     <div id ="profiles">
         <div id = "Angel" class="floater">
             <a href="../src/angel.php"><img src="../imgs/Angel.png" style="height:250px; width:200px; display:block; margin:auto;"> <p>Angel</p>
         </div>
@@ -26,8 +26,15 @@
             <a href="../src/kyra.php"><img src="../imgs/Kyra.jpg" style="height:250px; width:200px; display:block; margin:auto;"><p>Kyra</p></a>
         </div>
         <div id = "Spencer" class="floater">
-        <a href="../src/spencer.php"><img src="../imgs/Spencer.png" style="height:250px; width:200px; display:block; margin:auto;"><p>Spencer</p> 
+        <a href="../src/spencer.php"><img src="../imgs/Spencer.png" style="height:250px; width:200px; display:block; margin:auto;"><p>Spencer</p></a>
         </div>
+        
+    </div>  
+
+    <div class="floater" style = "text-align: left">
+        <h3>Dogs</h3>
+
+        
         <div id = "Moose" class="floater">
             <a href="../src/moose.php"><img src="../imgs/moose.jpg" style="height:250px; width:200px; display:block; margin:auto;"> <p>Moose</p>
         </div>
@@ -37,11 +44,13 @@
         <div id = "Remus" class="floater">
             <a href="../src/remus.php"><img src="../imgs/remus.JPEG" style="height:250px; width:200px; display:block; margin:auto;"> <p>Remus</p></a>
         </div>
-</div>
+    </div>
 
-<footer>
-    <p> Do you want to see your pet on this page? <a class="nav-item nav-link" href="petapplications.php">Click here</a> to fill out the form! </p>
-</footer>
+
+    <footer>
+        <p> Do you want to see your pet on this page? <a class="nav-item nav-link" href="petapplications.php">Click here</a> to fill out the form! </p>
+    </footer>
+</div>
 
 </body>
 

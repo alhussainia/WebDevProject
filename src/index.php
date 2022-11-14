35105b04fb79e4671e9a06d7cb1f987b60191ea7
knappenbergerk <knappenbergerk@etown.edu>
2022-11-14 11:50:30 -0500

    Made it so the words would not go behind our logo

--- src/index.php
+++ src/index.php
@@ -11,12 +11,12 @@
 <body>
 
     <div>
-    <a href=index.php><img src="../imgs/pet_profiles_no_background.png" alt="logo" id="logo" style="float:left; position: absolute; margin-top: -20px; height: 150px; width: 200px;"><a>
+    <a href=index.php><img src="../imgs/pet_profiles_no_background.png" alt="logo" id="logo" style="float:left; position: relative; margin-top: -20px; height: 150px; width: 200px;"><a>
     <h1>Elizabethtown College's Pet Profiles</h1>
     <?PHP require("navbar.php");?>
     </div>
 
-    <div style="width: 30%; float: left; margin-left: 50px; padding-top: 50px;">
+    <div style="width: 30%; float: left; margin-left: -140px; padding-top: 50px; position: float;">
         <h2> About Us: </h2>
             <p> The Elizabethtown College's Pet Profiles website was created for the students of ETown College and their pets.
                 This website will allow students with a Service Animal (SA), Emotional Support Animal (ESA), or an Assitance Animal (AA) to create

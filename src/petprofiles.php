7b10bf0c487423022671804237395b7975b89feb
knappenbergerk <knappenbergerk@etown.edu>
2022-11-30 11:23:11 -0500

    Removed the filler profiles

--- src/petprofiles.php
+++ src/petprofiles.php
@@ -21,37 +21,11 @@
     <h2> Pets On Campus</h2>
     <p> Here you can see all the pets that have had profiles created so far! </p>
     
-    <h3> Cats: </h3>
-    </div>
-
-     <div class="floater" id ="profiles" style = "text-align: left; position: absolute;">
-     <a href="../src/pets/angel.php"><div id = "Angel" class="floater">
-            <a href="../src/pets/angel.php"><img src="../imgs/Angel.png" style="height:250px; width:200px; display:block; margin:auto;"> <p>Angel</p></a>
-        </div></a>
-    <a href="../src/pets/kyra.php"><div id = "Kyra" class="floater">
-            <a href="../src/pets/kyra.php"><img src="../imgs/Kyra.jpg" style="height:250px; width:200px; display:block; margin:auto;"><p>Kyra</p></a>
-        </div></a>
-    <a href="../src/pets/spencer.php"><div id = "Spencer" class="floater">
-        <a href="../src/pets/spencer.php"><img src="../imgs/Spencer.png" style="height:250px; width:200px; display:block; margin:auto;"><p>Spencer</p></a>
-        </div></a>
-    </div>   
-    <div class="floater" style = "text-align: left; position: absolute; margin-top: 350px;">
-       
-    <h3>Dogs:</h3>
-
-    <a href="../src/pets/moose.php"><div id = "Moose" class="floater"></a>
-            <a href="../src/pets/moose.php"><img src="../imgs/moose.jpg" style="height:250px; width:200px; display:block; margin:auto;"> <p>Moose</p></a>
+    <div>
+        <h3>Pets: </h3>
         </div>
-    <a href="../src/pets/bubba.php"><div id = "Bubba" class="floater"></a>
-            <a href="../src/pets/bubba.php"><img src="../imgs/bubba.JPEG" style="height:250px; width:200px; display:block; margin:auto;"> <p>Bubba</p></a>
-        </div>
-    <a href="../src/pets/remus.php"><div id = "Remus" class="floater"></a>
-            <a href="../src/pets/remus.php"><img src="../imgs/remus.JPEG" style="height:250px; width:200px; display:block; margin:auto;"> <p>Remus</p></a>
-        </div>
-    </div>
 
-<div class="floater" style = "text-align: left; position: absolute; margin-top: 700px;">       
-<h3>Other Pets:</h3>
+    
 
 <?php
 require_once("functions/database_functions.php");    
